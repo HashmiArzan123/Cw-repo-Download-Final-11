4f3e52310305e1980134e44c8698acd312bf5214
arzaan <xdarzanplug@gmail.com>
2024-04-15 22:55:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6844922552:AAEeXWgjAUPZr_Y-pu1k3KcLm4Jil4yvmWU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7132588270:AAF02z_Y-E4GBKDoWjijvaELoH2caBGkZqk")
     API_ID = int(os.environ["API_ID", 4942197]
     API_HASH = os.environ["API_HASH", "13248a2c551b73193969b42194023635"]
     AUTH_USERS = "5892781710 """
